49a1b3dddc167b4162cf8ddca57e92c1bb101624
Oliver Ruoff <oliver93ruoff@web.de>
2021-01-11 17:14:28 +0100

    Added logs, fixed registering new uid

--- code/main.py
+++ code/main.py
@@ -12,53 +12,77 @@ REGISTERED_UIDS_FILE = '.registered_uids.txt'
 LOG_FILE = 'log.txt'
 
 
-def register_new_uid():
+def save_new_uid(uid):
+    inout.append_text_line_to_file(
+        LOG_FILE,
+        str(datetime.now) + ' Registered new uid: ' + uid)
+    inout.append_text_line_to_file(REGISTERED_UIDS_FILE, uid)
+    registered_uids.append(uid)
+    print('Successfully registered new uid:', uid)
+
+
+def register_first_key_as_master(uid):
+    stepper.turn_stepper(20, True)
+    time.sleep(0.2)
+    stepper.turn_stepper(20, False)
+    time.sleep(0.2)
+    save_new_uid(uid)
     for _ in range(2):
         stepper.turn_stepper(20, True)
         time.sleep(0.2)
         stepper.turn_stepper(20, False)
         time.sleep(0.2)
-        id, _ = rfid.read_rfid()
-        if id not in registered_uids:
-            inout.append_text_line_to_file(
-                LOG_FILE,
-                str(datetime.now) + ' Registered new id: ' + id)
-            inout.append_text_line_to_file(REGISTERED_UIDS_FILE, id)
-            registered_uids.append(id)
-        for _ in range(3):
-            stepper.turn_stepper(20, True)
-            time.sleep(0.2)
-            stepper.turn_stepper(20, False)
-            time.sleep(0.2)
+
+
+def register_new_uid():
+    stepper.turn_stepper(20, True)
+    time.sleep(0.2)
+    stepper.turn_stepper(20, False)
+    time.sleep(0.2)
+    uid, _ = rfid.read_rfid()
+    if uid not in registered_uids:
+        save_new_uid(uid)
+    else:
+        print('Uid is already registered!')
+    for _ in range(2):
+        stepper.turn_stepper(20, True)
+        time.sleep(0.2)
+        stepper.turn_stepper(20, False)
+        time.sleep(0.2)
 
 
 if __name__ == "__main__":
     try:
         registered_uids = inout.read_lines_from_file(REGISTERED_UIDS_FILE)
+        print('Registered uids:', registered_uids)
         while True:
-            id, text = rfid.read_rfid()
-            print(id, id in registered_uids)
-            if len(registered_uids) == 0 or id == registered_uids[0]:
-                print('Master key recognized!')
+            print('Listening to RFID module...')
+            uid, text = rfid.read_rfid()
+            print(uid, uid in registered_uids)
+            if len(registered_uids) == 0:
+                print('No uids registered yet, registering new (master) key.')
+                register_first_key_as_master(uid)
+            elif uid == registered_uids[0]:
+                print('Master key recognized! Registering new uid...')
                 register_new_uid()
-            if id in registered_uids:
+            if uid in registered_uids:
                 print('Unlocked!')
                 inout.append_text_line_to_file(
                     LOG_FILE,
-                    str(datetime.now) + ' Opened lock with id: ' + id)
+                    str(datetime.now) + ' Opened lock with uid: ' + uid)
                 stepper.turn_stepper(90, True)
                 time.sleep(0.5)
                 for _ in range(2):
-                    stepper.turn_stepper(10, False)
+                    stepper.turn_stepper(30, False)
                     time.sleep(0.5)
-                    stepper.turn_stepper(10, True)
+                    stepper.turn_stepper(30, True)
                     time.sleep(0.5)
                 stepper.turn_stepper(90, False)
             else:
                 print('Unauthorized! Sorry :P')
                 inout.append_text_line_to_file(
                     LOG_FILE,
-                    str(datetime.now) + ' Refused to open lock with id: ' + id)
+                    str(datetime.now) + ' Refused to open lock with uid: ' + uid)
                 for _ in range(5):
                     stepper.turn_stepper(5, True)
                     time.sleep(0.2)
